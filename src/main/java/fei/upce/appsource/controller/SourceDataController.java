403e06a06110ac87e51ea5a16d443155121b7f97
jirinovotny <hercingo@gmail.com>
2023-12-29 13:00:23 +0100

    add Rate limiting for whole project

--- src/main/java/fei/upce/appsource/controller/SourceDataController.java
+++ src/main/java/fei/upce/appsource/controller/SourceDataController.java
@@ -1,5 +1,6 @@
 package fei.upce.appsource.controller;
 
+import com.google.common.util.concurrent.RateLimiter;
 import fei.upce.appsource.Entity.SourceData;
 import fei.upce.appsource.service.SourceDataService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,13 +20,18 @@ public class SourceDataController {
     @Autowired
     private SourceDataService sourceDataService;
 
+    private final RateLimiter rateLimiter = RateLimiter.create(0.1667); // 10 per minute
     @GetMapping("/getAllData")
     @PreAuthorize("hasRole('READER') or hasRole('FULL') or hasRole('ADMIN')")
     public List<Optional<SourceData>> showAllSourceData() {
+        if (rateLimiter.tryAcquire()) {
         try {
             return sourceDataService.getSourceData();
         }catch (Exception e){
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        } else {
+            throw new ResponseStatusException(HttpStatus.TOO_MANY_REQUESTS, "Too many requests");
         }
     }
 }
